8e8ed172095178781d56614dcd3a9b9ab4248571
Язиков Степан <cnikia@iMac-admin.local>
2020-06-11 18:04:16 +0300

    .

--- api_test/settings.py
+++ api_test/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'api_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'api_test',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
